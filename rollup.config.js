df95f9d602da5b043303226ae797ee0510503d1d
mpyw <ryosuke_i_628@yahoo.co.jp>
2020-04-09 17:38:06 +0900

    Support UMD build (#25)

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import nodeResolve from "@rollup/plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import { terser } from "rollup-plugin-terser";
+
+const isProd = process.env.NODE_ENV === "production";
+
+const config = {
+  input: "src/index.ts",
+  output: {
+    format: "umd",
+    name: "AxiosCaseConverter",
+    sourcemap: true,
+    compact: isProd,
+    indent: !isProd,
+    exports: "named",
+    file: `dist/axios-case-converter.${isProd ? "min.js" : "js"}`,
+  },
+  plugins: [
+    nodeResolve({
+      extensions: [".ts"],
+    }),
+    babel({
+      include: ["src/**/*"],
+      exclude: "node_modules/**",
+      extensions: [".ts"],
+    }),
+    isProd && terser(),
+  ],
+};
+
+export default config;
