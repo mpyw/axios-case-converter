e7a0b388d3e91add96c9bf87127ea784baec73b9
mpyw <ryosuke_i_628@yahoo.co.jp>
2021-07-02 18:54:40 +0900

    test: 💍 Fix broken tests

--- test/integration/basic.ts
+++ test/integration/basic.ts
@@ -1,28 +1,26 @@
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
-import applyCaseMiddleware from "../../src";
-
-/* eslint-disable @typescript-eslint/camelcase */
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import applyCaseMiddleware from '../../src';
 
 const snakeData = {
   user_id: 1,
   something_object: {
-    foo_bar_baz123: "fooBarBaz123",
-    this_snake_should_be_preserved: "preserved",
-    thisCamelShouldBePreserved: "preserved",
+    foo_bar_baz123: 'fooBarBaz123',
+    this_snake_should_be_preserved: 'preserved',
+    thisCamelShouldBePreserved: 'preserved',
   },
-  something_array: ["foo", { foo_bar_baz456: "fooBarBaz456" }],
+  something_array: ['foo', { foo_bar_baz456: 'fooBarBaz456' }],
   entries: {
-    foo_bar_baz123: "fooBarBaz123",
+    foo_bar_baz123: 'fooBarBaz123',
   },
   append: {
-    foo_bar_baz123: "fooBarBaz123",
+    foo_bar_baz123: 'fooBarBaz123',
   },
   constructor: {
-    foo_bar_baz123: "fooBarBaz123",
+    foo_bar_baz123: 'fooBarBaz123',
   },
   prototype: {
-    foo_bar_baz123: "fooBarBaz123",
+    foo_bar_baz123: 'fooBarBaz123',
   },
   empty: null,
 };
@@ -30,75 +28,75 @@ const snakeData = {
 const camelData = {
   userId: 1,
   somethingObject: {
-    fooBarBaz123: "fooBarBaz123",
-    this_snake_should_be_preserved: "preserved",
-    thisCamelShouldBePreserved: "preserved",
+    fooBarBaz123: 'fooBarBaz123',
+    this_snake_should_be_preserved: 'preserved',
+    thisCamelShouldBePreserved: 'preserved',
   },
-  somethingArray: ["foo", { fooBarBaz456: "fooBarBaz456" }],
+  somethingArray: ['foo', { fooBarBaz456: 'fooBarBaz456' }],
   entries: {
-    fooBarBaz123: "fooBarBaz123",
+    fooBarBaz123: 'fooBarBaz123',
   },
   append: {
-    fooBarBaz123: "fooBarBaz123",
+    fooBarBaz123: 'fooBarBaz123',
   },
   constructor: {
-    fooBarBaz123: "fooBarBaz123",
+    fooBarBaz123: 'fooBarBaz123',
   },
   prototype: {
-    fooBarBaz123: "fooBarBaz123",
+    fooBarBaz123: 'fooBarBaz123',
   },
   empty: null,
 };
 
 const client = applyCaseMiddleware(
   axios.create({
-    baseURL: "http://example.com",
+    baseURL: 'http://example.com',
   }),
   {
     preservedKeys: [
-      "this_snake_should_be_preserved",
-      "thisCamelShouldBePreserved",
-      "THIS-HEADER-SHOULD-BE-PRESERVED",
+      'this_snake_should_be_preserved',
+      'thisCamelShouldBePreserved',
+      'THIS-HEADER-SHOULD-BE-PRESERVED',
     ],
   }
 );
 const mock = new MockAdapter(client);
 
-test("it should be converted on success", (done) => {
-  mock.onPost("/success").reply((config) => {
-    expect(config.method).toBe("post");
-    expect(config.headers["X-Requested-With"]).toBe("XMLHttpRequest");
-    expect(config.headers.thisCamelShouldBePreserved).toBe("preserved");
+test('it should be converted on success', (done) => {
+  mock.onPost('/success').reply((config) => {
+    expect(config.method).toBe('post');
+    expect(config.headers['X-Requested-With']).toBe('XMLHttpRequest');
+    expect(config.headers.thisCamelShouldBePreserved).toBe('preserved');
     expect(config.params.user_id).toBe(1);
-    expect(config.params.screen_name).toBe("yay");
-    expect(config.params.thisCamelShouldBePreserved).toBe("preserved");
+    expect(config.params.screen_name).toBe('yay');
+    expect(config.params.thisCamelShouldBePreserved).toBe('preserved');
     expect(config.data).toBe(JSON.stringify(snakeData));
     return [
       200,
       snakeData,
       {
-        "Content-Type": "application/json",
-        "THIS-HEADER-SHOULD-BE-PRESERVED": "preserved",
+        'Content-Type': 'application/json',
+        'THIS-HEADER-SHOULD-BE-PRESERVED': 'preserved',
       },
     ];
   });
   client
-    .post("/success", camelData, {
+    .post('/success', camelData, {
       headers: {
-        xRequestedWith: "XMLHttpRequest",
-        thisCamelShouldBePreserved: "preserved",
+        xRequestedWith: 'XMLHttpRequest',
+        thisCamelShouldBePreserved: 'preserved',
       },
       params: {
         userId: 1,
-        screenName: "yay",
-        thisCamelShouldBePreserved: "preserved",
+        screenName: 'yay',
+        thisCamelShouldBePreserved: 'preserved',
       },
     })
     .then((response) => {
       expect(JSON.stringify(response.data)).toBe(JSON.stringify(camelData));
-      expect(response.headers.contentType).toBe("application/json");
-      expect(response.headers["THIS-HEADER-SHOULD-BE-PRESERVED"]).toBe(
-        "preserved"
+      expect(response.headers.contentType).toBe('application/json');
+      expect(response.headers['THIS-HEADER-SHOULD-BE-PRESERVED']).toBe(
+        'preserved'
       );
       done();
     })
@@ -107,47 +105,47 @@ test("it should be converted on success", (done) => {
     });
 });
 
-test("it should be converted on failure", (done) => {
-  mock.onPost("/failure").reply((config) => {
-    expect(config.method).toBe("post");
-    expect(config.headers["X-Requested-With"]).toBe("XMLHttpRequest");
-    expect(config.headers.thisCamelShouldBePreserved).toBe("preserved");
+test('it should be converted on failure', (done) => {
+  mock.onPost('/failure').reply((config) => {
+    expect(config.method).toBe('post');
+    expect(config.headers['X-Requested-With']).toBe('XMLHttpRequest');
+    expect(config.headers.thisCamelShouldBePreserved).toBe('preserved');
     expect(config.params.user_id).toBe(1);
-    expect(config.params.screen_name).toBe("yay");
-    expect(config.params.thisCamelShouldBePreserved).toBe("preserved");
+    expect(config.params.screen_name).toBe('yay');
+    expect(config.params.thisCamelShouldBePreserved).toBe('preserved');
     expect(config.data).toBe(JSON.stringify(snakeData));
     return [
       400,
       snakeData,
       {
-        "Content-Type": "application/json",
-        "THIS-HEADER-SHOULD-BE-PRESERVED": "preserved",
+        'Content-Type': 'application/json',
+        'THIS-HEADER-SHOULD-BE-PRESERVED': 'preserved',
       },
     ];
   });
   client
-    .post("/failure", camelData, {
+    .post('/failure', camelData, {
       headers: {
-        xRequestedWith: "XMLHttpRequest",
-        thisCamelShouldBePreserved: "preserved",
+        xRequestedWith: 'XMLHttpRequest',
+        thisCamelShouldBePreserved: 'preserved',
       },
       params: {
         userId: 1,
-        screenName: "yay",
-        thisCamelShouldBePreserved: "preserved",
+        screenName: 'yay',
+        thisCamelShouldBePreserved: 'preserved',
       },
     })
     .then(
       () => {
-        done(new Error("Error has not been occurred."));
+        done(new Error('Error has not been occurred.'));
       },
       (error) => {
         expect(JSON.stringify(error.response.data)).toBe(
           JSON.stringify(camelData)
         );
-        expect(error.response.headers.contentType).toBe("application/json");
-        expect(error.response.headers["THIS-HEADER-SHOULD-BE-PRESERVED"]).toBe(
-          "preserved"
+        expect(error.response.headers.contentType).toBe('application/json');
+        expect(error.response.headers['THIS-HEADER-SHOULD-BE-PRESERVED']).toBe(
+          'preserved'
         );
         done();
       }
--- test/integration/internet-explorer.ts
+++ test/integration/internet-explorer.ts
@@ -1,22 +1,25 @@
-import { noCase } from "no-case";
-import { createObjectTransformer } from "../../src/transformers";
+// noinspection JSConstantReassignment
 
-let warn: Console["warn"];
+import { noCase } from 'no-case';
+import { createObjectTransformer } from '../../src/transformers';
+
+let warn: Console['warn'];
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 
 beforeEach(() => {
-  /* eslint-disable @typescript-eslint/ban-ts-ignore */
   // @ts-ignore
-  global.Blob = require("blob-polyfill").Blob;
+  global.Blob = require('blob-polyfill').Blob;
   // @ts-ignore
-  global.navigator = { product: "Gecko" };
-  /* eslint-enable @typescript-eslint/ban-ts-ignore */
-  require("formdata-polyfill");
-  require("url-search-params-polyfill");
+  global.navigator = { product: 'Gecko' };
+  // @ts-ignore
+  require('formdata-polyfill');
+  require('url-search-params-polyfill');
   warn = console.warn;
 });
 
 afterEach(() => {
-  /* eslint-disable @typescript-eslint/ban-ts-ignore */
   // @ts-ignore
   delete global.Blob;
   // @ts-ignore
@@ -25,28 +28,31 @@ afterEach(() => {
   delete global.FormData;
   // @ts-ignore
   delete global.URLSearchParams;
-  /* eslint-enable @typescript-eslint/ban-ts-ignore */
   console.warn = warn;
   jest.resetModules();
 });
 
-test("it should warn about FormData.prototype.entries() in ReactNative", () => {
+test('it should warn about FormData.prototype.entries() in ReactNative', () => {
   console.warn = jest.fn();
 
+  // @ts-ignore
   const entries = FormData.prototype.entries;
+  // @ts-ignore
   delete FormData.prototype.entries;
   createObjectTransformer(noCase)(new FormData());
 
   expect(console.warn).toBeCalledWith(
-    "You must use polyfill of FormData.prototype.entries() on Internet Explorer or Safari: https://github.com/jimmywarting/FormData"
+    'You must use polyfill of FormData.prototype.entries() on Internet Explorer or Safari: https://github.com/jimmywarting/FormData'
   );
+  // @ts-ignore
   FormData.prototype.entries = entries;
 });
 
-test("it should not warn about FormData.prototype.delete() when overwriting disabled in ReactNative", () => {
+test('it should not warn about FormData.prototype.delete() when overwriting disabled in ReactNative', () => {
   console.warn = jest.fn();
 
   const delete_ = FormData.prototype.delete;
+  // @ts-ignore
   delete FormData.prototype.delete;
   createObjectTransformer(noCase)(new FormData());
 
@@ -54,36 +60,41 @@ test("it should not warn about FormData.prototype.delete() when overwriting disa
   FormData.prototype.delete = delete_;
 });
 
-test("it should warn about FormData.prototype.delete() when overwriting enabled in ReactNative", () => {
+test('it should warn about FormData.prototype.delete() when overwriting enabled in ReactNative', () => {
   console.warn = jest.fn();
 
   const delete_ = FormData.prototype.delete;
+  // @ts-ignore
   delete FormData.prototype.delete;
   createObjectTransformer(noCase)(new FormData(), { overwrite: true });
 
   expect(console.warn).toBeCalledWith(
-    "You must use polyfill of FormData.prototype.delete() on Internet Explorer or Safari: https://github.com/jimmywarting/FormData"
+    'You must use polyfill of FormData.prototype.delete() on Internet Explorer or Safari: https://github.com/jimmywarting/FormData'
   );
   FormData.prototype.delete = delete_;
 });
 
-test("it should warn about URLSearchParams.prototype.entries() in ReactNative", () => {
+test('it should warn about URLSearchParams.prototype.entries() in ReactNative', () => {
   console.warn = jest.fn();
 
+  // @ts-ignore
   const entries = URLSearchParams.prototype.entries;
+  // @ts-ignore
   delete URLSearchParams.prototype.entries;
   createObjectTransformer(noCase)(new URLSearchParams());
 
   expect(console.warn).toBeCalledWith(
-    "You must use polyfill of URLSearchParams.prototype.entries() on Internet Explorer or Safari: https://github.com/jerrybendy/url-search-params-polyfill"
+    'You must use polyfill of URLSearchParams.prototype.entries() on Internet Explorer or Safari: https://github.com/jerrybendy/url-search-params-polyfill'
   );
+  // @ts-ignore
   URLSearchParams.prototype.entries = entries;
 });
 
-test("it should not warn about URLSearchParams.prototype.delete() when overwriting disabled in ReactNative", () => {
+test('it should not warn about URLSearchParams.prototype.delete() when overwriting disabled in ReactNative', () => {
   console.warn = jest.fn();
 
   const delete_ = URLSearchParams.prototype.delete;
+  // @ts-ignore
   delete URLSearchParams.prototype.delete;
   createObjectTransformer(noCase)(new URLSearchParams());
 
@@ -91,15 +102,16 @@ test("it should not warn about URLSearchParams.prototype.delete() when overwriti
   URLSearchParams.prototype.delete = delete_;
 });
 
-test("it should warn about URLSearchParams.prototype.delete() when overwriting enabled in ReactNative", () => {
+test('it should warn about URLSearchParams.prototype.delete() when overwriting enabled in ReactNative', () => {
   console.warn = jest.fn();
 
   const delete_ = URLSearchParams.prototype.delete;
+  // @ts-ignore
   delete URLSearchParams.prototype.delete;
   createObjectTransformer(noCase)(new URLSearchParams(), { overwrite: true });
 
   expect(console.warn).toBeCalledWith(
-    "You must use polyfill of URLSearchParams.prototype.delete() on Internet Explorer or Safari: https://github.com/jerrybendy/url-search-params-polyfill"
+    'You must use polyfill of URLSearchParams.prototype.delete() on Internet Explorer or Safari: https://github.com/jerrybendy/url-search-params-polyfill'
   );
   URLSearchParams.prototype.delete = delete_;
 });
